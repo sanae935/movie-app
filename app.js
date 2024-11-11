5134330d3a7375177ae3628e57ada22fbca66967
sanae935 <s.elatmani005@gmail.com>
2024-11-11 00:06:09 -0500

    pushing file

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
-const movieRoutes = require('./routes/movieRoutes');
-const reviewRoutes = require('./routes/reviewRoutes');
+dotenv.config(); 
+
+const mongoURI = process.env.MONGO_URI;
 
 const app = express();
+
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/movie-review-app', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((err) => console.log('MongoDB connection error:', err));
+  
+  app.get('/', (req, res) => {
+    res.send('Welcome to the Movie Review API');
+  });
+  import movieRoutes from './routes/movieRoutes.js';
+  app.use('/api/movies', movieRoutes);
+ 
 
-app.use('/api/movies', movieRoutes);
-app.use('/api/reviews', reviewRoutes);
-
-const PORT = 5000;
+  const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
