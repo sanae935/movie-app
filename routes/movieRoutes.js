675f969f7141b6b9e82ab01807874419f32ce134
sanae935 <s.elatmani005@gmail.com>
2024-11-10 21:41:09 -0500

    pushing file

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -10,3 +10,16 @@ router.get('/', async (req, res) => {
       res.status(500).send('Error fetching movies');
     }
   });
+  router.post('/', async (req, res) => {
+    const { title, genre } = req.body;
+  
+    try {
+      const newMovie = new Movie({ title, genre });
+      await newMovie.save();
+      res.status(201).json(newMovie);
+    } catch (err) {
+      res.status(400).send('Error adding movie');
+    }
+  });
+  
+  module.exports = router;
