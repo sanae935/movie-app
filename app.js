19d9a4ef25c76e8a8178c4c612989bcb2dff206a
sanae935 <s.elatmani005@gmail.com>
2024-11-10 22:27:22 -0500

    pushing file

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const bodyParser = require('body-parser');
 
 const movieRoutes = require('./routes/movieRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/movie-review-app', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('MongoDB connection error:', err));
