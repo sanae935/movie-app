92d954d9c39f7a9bd5f1b16358fe85b4cf261642
sanae935 <s.elatmani005@gmail.com>
2024-11-10 22:00:28 -0500

    pushing file

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Review = require('../models/Review');
+
+router.get('/:movieId', async (req, res) => {
+    try {
+      const reviews = await Review.find({ movieId: req.params.movieId });
+      res.json(reviews);
+    } catch (err) {
+      res.status(500).send('Error fetching reviews');
+    }
+  });
+  
