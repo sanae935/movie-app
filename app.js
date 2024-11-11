842610a17788eaaf9147f9981ad526fd1829206c
sanae935 <s.elatmani005@gmail.com>
2024-11-10 22:13:21 -0500

    pushing file

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const movieRoutes = require('./routes/movieRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
