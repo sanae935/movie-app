4eccfc5de8d4a4c7dac17d9ba70903cdb33b364e
sanae935 <s.elatmani005@gmail.com>
2024-11-10 22:08:23 -0500

    pushing file

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -10,4 +10,16 @@ router.get('/:movieId', async (req, res) => {
       res.status(500).send('Error fetching reviews');
     }
   });
+  router.post('/', async (req, res) => {
+    const { movieId, rating, text } = req.body;
   
+    try {
+      const newReview = new Review({ movieId, rating, text });
+      await newReview.save();
+      res.status(201).json(newReview);
+    } catch (err) {
+      res.status(400).send('Error adding review');
+    }
+  });
+  
+  module.exports = router;
