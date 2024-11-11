b4db5b11437fb53346a4336ba8e1a18a1ee28bbd
sanae935 <s.elatmani005@gmail.com>
2024-11-10 23:30:07 -0500

    pushing file

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 import movieRoutes from './routes/movieRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
@@ -8,7 +11,8 @@ import reviewRoutes from './routes/reviewRoutes.js';
 const app = express();
 app.use(bodyParser.json());
 
-const dbURI = 'mongodb+srv://sanaa:aicha123456@cluster0.kg26p.mongodb.net/';
+const dbURI = process.env.MONGO_URI;
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((err) => console.log('MongoDB connection error:', err));
